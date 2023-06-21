1bfc9fd2c811583a276f8a2e03f047f63d7a6d62
LFWQSP2641 <liuyunbo2641@gmail.com>
2023-06-22 00:29:39 +0800

    perf.对Windows端也加以限制

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "GUI/MainWidget.h"
+#include "Logic/UpdateChecker.h"
 #include "StaticClass/Global.h"
 #include "StaticClass/Setting.h"
 #include "Singleton/Network.h"
@@ -32,7 +33,18 @@ int main(int argc, char *argv[])
     }
 
     MainWidget w;
+    w.setEnabled(false);
     w.show();
+    if(UpdateChecker().checkMinimumVersion())
+    {
+        w.setEnabled(true);
+    }
+    else
+    {
+        QMessageBox::critical(&w, QStringLiteral("版本过低"), QStringLiteral("请更新版本或检查网络连接"));
+        a.exit(1);
+        return 1;
+    }
 
     return a.exec();
 }
